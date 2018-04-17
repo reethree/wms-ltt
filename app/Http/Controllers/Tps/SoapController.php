8535eb7d4a4d3fd0d46e5e8ab0354a88084367d3
Reza R Ramadhan <reza@contrivent.com>
2018-04-17 12:28:45 +0700

    UPDATE TPS ONLINE

--- app/Http/Controllers/Tps/SoapController.php
+++ app/Http/Controllers/Tps/SoapController.php
@@ -559,8 +559,8 @@ class SoapController extends DefaultController {
                 ->name('TpsOnline_GetSPJM')
                 ->wsdl($this->wsdl)
                 ->trace(true)                                                                                                  
-//                ->certificate(url('cert/bc.pem'))  
-                ->certificate(url('cert/tpsonlinebc.crt')) 
+                ->certificate(url('cert/bc.pem'))  
+//                ->certificate(url('cert/tpsonlinebc.crt')) 
 //                ->certificate(url('cert/trust-ca.crt')) 
                 ->cache(WSDL_CACHE_NONE);                                                     
         });
