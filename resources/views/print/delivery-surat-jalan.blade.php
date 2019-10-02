d95bdc746984daf7e8ec26473e471ee6faeba372
Reza R Ramadhan <reza@contrivent.com>
2019-10-02 15:38:29 +0700

    Update

--- resources/views/print/delivery-surat-jalan.blade.php
+++ resources/views/print/delivery-surat-jalan.blade.php
@@ -30,7 +30,7 @@
                         <tr>
                             <td>Dari Gudang / Lap</td>
                             <td class="padding-10 text-center">:</td>
-                            <td>PT. Lautan Tirta Transportama</td>
+                            <td>PT. LAUTAN TIRTA TRASPORTAMA</td>
                         </tr>
                         <tr>
                             <td>Ex. Kapal</td>
@@ -42,54 +42,23 @@
                             <td class="padding-10 text-center">:</td>
                             <td>{{ date("d F Y", strtotime($manifest->ETA)) }}</td>
                         </tr>
-<!--                        <tr>
-                            <td>Kepada Yth.</td>
-                            <td class="padding-10 text-center">:</td>
-                            <td>{{ $manifest->NAMACONSOLIDATOR }}</td>
-                        </tr>
-                        <tr>
-                            <td>No. Surat Jalan </td>
-                            <td class="padding-10 text-center">:</td>
-                            <td>{{ $manifest->NOTALLY }}</td>
-                        </tr>
-                        <tr>
-                            <td>Ex. Kapal</td>
-                            <td class="padding-10 text-center">:</td>
-                            <td>{{ $manifest->VESSEL }}</td>
-                        </tr>
-                        <tr>
-                            <td>Voy</td>
-                            <td class="padding-10 text-center">:</td>
-                            <td>{{ $manifest->VOY }}</td>
-                        </tr>
                         <tr>
                             <td>No. HBL </td>
                             <td class="padding-10 text-center">:</td>
                             <td>{{ $manifest->NOHBL }}</td>
                         </tr>
                         <tr>
-                            <td>No. Truck</td>
+                            <td>Truck No. Pol</td>
                             <td class="padding-10 text-center">:</td>
                             <td>{{ $manifest->NOPOL }}</td>
-                        </tr>-->
+                        </tr>
 
-                    </table>
-                    <table border="0" cellspacing="0" cellpadding="0">
-                        <tr>
-                            <td style="border: 1px solid;">&nbsp;&nbsp;</td>
-                            <td>Barang dalam keadaan baik, lengkap dan sesuai DO.</td>
-                        </tr>
-                        <tr><td style="border-bottom: 1px solid;"></td><td></td></tr>
-                        <tr>
-                            <td style="border: 1px solid;">&nbsp;&nbsp;</td>
-                            <td>Barang dalam keadaan rusak/cacat/tidak lengkap (Lampiran berita acara)</td>
-                        </tr>
                     </table>
                 </td>
                 <td style="vertical-align: top;">
                     <table border="0" cellspacing="0" cellpadding="0" style="font-size: 12px;">
                         <tr>
-                            <td>DIKIRIM KEPADA</td>
+                            <td>Dikirim Kepada :</td>
                         </tr>
                         <tr>
                             <td>Yth. {{ $manifest->CONSIGNEE }}</td>
@@ -103,7 +72,7 @@
         <table border="1" cellspacing="0" cellpadding="0">
             <thead>
                 <tr>
-                    <th rowspan="2">MERK / No.</th>
+                    <th rowspan="2">JENIS<br />DOKUMEN</th>
                     <th rowspan="2">JENIS BARANG</th>
                     <th colspan="2">JUMLAH BARANG</th>
                     <th rowspan="2">KETERANGAN</th>
@@ -115,7 +84,7 @@
             </thead>
             <tbody>
                 <tr>
-                    <td style="width: 15%;vertical-align: top;">{{ $manifest->NAMACONSOLIDATOR }}</td>
+                    <td style="width: 15%;vertical-align: top;">{{ $manifest->KODE_DOKUMEN }}<br/>{{$manifest->NO_SPPB.' '.$manifest->TGL_SPPB}}</td>
                     <td style="width: 40%;vertical-align: top;">{{ $manifest->MARKING }}</td>
                     <td style="width: 10%;text-align: center;vertical-align: top;">{{ $manifest->QUANTITY }}/{{ $manifest->NAMAPACKING }}</td>
                     <td style="width: 10%;text-align: center;vertical-align: top;">{{ $manifest->MEAS }}</td>
@@ -126,15 +95,25 @@
               
         <table border="0" cellspacing="0" cellpadding="0">
             <tr>
-                <td style="text-align: right;">Tanjung Priok, {{ date('d F Y') }}</td>
+                <td style="border: 1px solid;">&nbsp;&nbsp;</td>
+                <td>Barang dalam keadaan baik, lengkap dan sesuai DO.</td>
+            </tr>
+            <tr><td style="border-bottom: 1px solid;"></td><td></td></tr>
+            <tr>
+                <td style="border: 1px solid;">&nbsp;&nbsp;</td>
+                <td>Barang dalam keadaan rusak/cacat/tidak lengkap (Lampiran berita acara)</td>
+            </tr>
+            <tr>
+                <td colspan="2" style="padding: 0;height: 50px;"><span style="border: 2px solid;padding: 6px;font-size: 10px;"><b>KOMPLAIN HANYA DILAYANI PADA HARI & TANGGAL YANG SAMA</b></span></td>
             </tr>
         </table>
+        
         <!--<div style="page-break-after: always;"></div>-->
         <table border="0" cellspacing="0" cellpadding="0">
             <tr>
-                <td>Penerima,<br />Nama Terang / Stempel Perusahaan</td>
-                <td>Staff ADM.,<br />Nama Terang</td>
-                <td style="text-align: center;">PT. LAUTAN TIRTA TRANSPORTAMA<br />Kepala Gudang</td>
+                <td>Penerima,<br />Nama Terang/EMKL/Importir</td>
+                <td>Staff Adm,<br />Nama Terang</td>
+                <td style="text-align: center;">Tanjung Priok, {{ date('d F Y') }}<br />PT. LAUTAN TIRTA TRANSPORTAMA<br />Kepala Gudang</td>
             </tr>
 <!--            <tr>
                 <td>&nbsp;</td>
